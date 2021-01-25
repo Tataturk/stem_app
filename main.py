9c149e595c067226ca6ab57a6cd5b621a7966bb3
Erencan <erencankanmaz110@live.nl>
2021-01-25 13:48:29 +0100

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+#TODO
+# Registratie. 
+# Dubbel stemmen voorkomen. List van stemmers die hebben gestemd.
+
+#TODO
+# Anonimiteit
+# Elk persoon anoniem stemmen. Niet terug kunnen koppelen.
+
+#TODO
+# Geheimhouding.
+# Opgeslagen data is beveiligd.
+
+#TODO
+# Bewijsbaarheid
+# Betrouwbare gegevens een hertelling
+
+#TODO
+# Realisatie
+# Werkend product opleveren
+
+#TODO
+# Format stemmen:
+# stem -p -k
+# -p: persoon
+# -k: keuze
+#
+# --new: start met stemmen.
+# --res: display tussenresultaten
+# --del: verwijder alle informatie
